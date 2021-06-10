d4c6d37b1cfd34cce2c3f376b9fad0652c359e94
Serdar Yegulalp <serdar@genjipress.com>
2021-06-09 21:30:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+This is a simple example of how to compile a Cython file to a standalone executable in Windows. There are very few good examples of how to do this with the most recent Visual Studio toolchain, so I thought I'd make this example for all who need it.
+
+It ought to be possible to use this as a base for a way to create simple redistributable Python applications that have no more than they need to run.
+
+This demo requires:
+
+* Python 3.8 or better (I haven't tested on anything less than that)
+* Visual Studio 2019 Community edition; specifically, the Visual C build tools
+* Cython
+
+> **Important:** In addition to having a Python version installed, you'll need to download the .ZIP-embeddable version of *the same version of Python used* and unpack it into a subdirectory named `embed`. This is so that the zipped copy of the standard library can be copied into the `dist` directory.
+
+You can install the Python requirements from `requirements.txt`.
+
+Run `make.py` to trigger the build process. The resulting executable, along with the Python .DLL and a minimal standard library, will be in a subdirectory named `dist`.
+
+`make.py` takes the following command-line flags:
+
+* `-w`: Build a windowed-only version of the application (no console output).
+* `-r`: Run the built `.exe` immediately after completing the build process.
+
+In time I may expand this example with more switches and options to better control how the standard library and other bundled components (e.g., SQLite3) are handled.
+
+This code is distributed under the MIT license.
---
+++ make.py
@@ -0,0 +1,61 @@
+# You may need to adjust this path on your system;
+# it assumes Visual Studio 2019's community build tools are installed
+
+vc_path = r"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
+win_kitpath = r"%WindowsSdkDir%Lib\%WindowsSDKVersion%um\%VSCMD_ARG_HOST_ARCH%"
+
+from pathlib import Path
+import shutil
+import subprocess
+import sys
+
+embed_src = Path("embed")
+dist_path = Path("dist")
+exec_path = sys.base_prefix
+
+
+def clean_files():
+    for f in ("main.exp", "main.lib", "main.obj", "main.c", "main.exe"):
+        Path(f).unlink(missing_ok=True)
+
+build_win = ""
+
+if "-w" in sys.argv:
+    print ("Building Win32 application")
+    build_win = "/subsystem:windows /entry:wmainCRTStartup"
+else:
+    print ("Building console application")
+
+cmd = rf"""cython --embed -3 main.pyx
+call "{vc_path}\vcvarsall.bat" x64
+cl main.c /I "{exec_path}\include" /link "{exec_path}\libs\python38.lib" "{win_kitpath}\User32.lib" "{win_kitpath}\Kernel32.lib" {build_win}
+"""
+
+clean_files()
+
+if dist_path.exists():
+    shutil.rmtree(dist_path)
+    dist_path.mkdir()
+
+console = subprocess.Popen(
+    "cmd.exe",
+    shell=False,
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+)
+out, err = console.communicate(bytes(cmd, encoding="utf8"))
+print(out.decode("utf-8"))
+
+shutil.move(str(Path("main.exe")), dist_path)
+
+clean_files()
+
+for f in ("python38.dll", "python38.zip"):
+    shutil.copy2(embed_src / f, dist_path)
+
+with open(dist_path / "python38._pth", "w") as f:
+    f.write("python38.zip")
+
+if "-r" in sys.argv:
+    subprocess.run(dist_path/"main.exe")
