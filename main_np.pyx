5dbaa65b04f945835ef3fc3948a4fc00e9c0bdc5
Serdar Yegulalp <serdar@genjipress.com>
2021-06-10 22:21:50 -0400

    details for how to make numpy import example work

--- main_np.pyx
+++ main_np.pyx
@@ -1,6 +1,10 @@
 # This demo prints a message to the console, and also opens a Win32 message box
 # It also attempts to import numpy, which won't work unless you have numpy and
-# all its stdlib dependencies in the distribution folder.
+# all its stdlib dependencies in the distribution folder. To make this work,
+# install numpy in your vent, download the "embeddable" version of Python (the
+# .zip archive), unpack that into a folder, copy the numpy folder from the venv's
+# site-packages directory into that folder, and then copy the executable generated
+# from this file also into that folder.
 
 cdef extern int MessageBoxA(long hwnd, char* text, char* caption, unsigned int type )
 
